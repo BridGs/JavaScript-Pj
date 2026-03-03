e0b52b1585cb8fdda1ea7118434eb9b55aa49fd6
BridGs <benjamincordobast@hotmail.com>
2026-03-03 17:04:52 -0300

    Corrected error to display on table

--- script.js
+++ script.js
@@ -7,10 +7,22 @@ const missionLevel = document.getElementById("level");
 const storedMissions = localStorage.getItem("missionList");
 
 let missionList = [];
-missionList = storedMissions;
+const missionLoader = {
+	id: 0,
+	title: "loader",
+	difficulty: "1.0",
+	level: "0",
+	state: "finished"
+};
+missionList.push(missionLoader);
+if(storedMissions){
+	missionList.push(storedMissions);
+}
 
+//Add mission to array
 missionCreateBtn.addEventListener("click",function (){
 	const mission = {
+		id: (missionList.length),
 		title: missionTitle,
 		difficulty: difficulty,
 		level: missionLevel,
@@ -22,4 +34,31 @@ missionCreateBtn.addEventListener("click",function (){
 	localStorage.setItem("missionList",JSON.stringify(missionList));
 
 	console.log(missionList);
+	renderMissionTable(missionList);
 });
+
+let tableBody = document.getElementById("missionTable")
+//Make table of mission from array
+function renderMissionTable (data) {
+	console.log(data);
+	tableBody.innerHTML = "";
+
+	data.forEach(mission => {
+		console.log(mission);
+		const row = document.createElement("tr");
+
+		const missionCell = document.createElement("td");
+		missionCell.textContent = mission.title;
+		const difficultyCell = document.createElement("td");
+		difficultyCell.textContent = mission.difficulty;
+		const levelCell = document.createElement("td");
+		levelCell.textContent = mission.level;
+	
+		row.appendChild(missionCell);
+		row.appendChild(difficultyCell);
+		row.appendChild(levelCell);
+		tableBody.appendChild(row);
+	});
+}
+
+renderMissionTable(missionList);
