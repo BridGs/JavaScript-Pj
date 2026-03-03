9739289429d76a603a8e70a2b9fcb09e48c4e365
BridGs <benjamincordobast@hotmail.com>
2026-03-03 13:16:03 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+//Mission data input
+const missionCreateBtn = document.getElementById("createMissionBtn");
+const missionTitle = document.getElementById("missionTitle");
+const difficulty = document.getElementById("difficulty");
+
+let missionList = [];
+
+missionCreateBtn.addEventListener("click",function (){
+	const mission = {
+		title: 
+	}
+})
