7d1833c6c9e7988058b59c18ae2bc7a66a6bc1c0
BridGs <benjamincordobast@hotmail.com>
2026-03-03 13:35:30 -0300

    Added localStorage to mission list

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ const missionCreateBtn = document.getElementById("createMissionBtn");
 const missionTitle = document.getElementById("missionTitle");
 const difficulty = document.getElementById("difficulty");
 const missionLevel = document.getElementById("level");
+//retrieving stored missionList
+const storedMissions = localStorage.getItem("missionList");
 
 let missionList = [];
+missionList = storedMissions;
 
 missionCreateBtn.addEventListener("click",function (){
 	const mission = {
@@ -15,6 +18,8 @@ missionCreateBtn.addEventListener("click",function (){
 	};
 
 	missionList.push(mission);
+	//converts to string then saves to local storage
+	localStorage.setItem("missionList",JSON.stringify(missionList));
 
 	console.log(missionList);
 });
